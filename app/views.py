c5fa4fd3d878132838e2b4c20feae43389da2052
Grant Curell <grantcurell@gmail.com>
2019-07-12 19:57:41 -0500

    Fixed another minor bug

--- app/views.py
+++ app/views.py
@@ -29,9 +29,8 @@ def _lookup_character():
         with open("word_searches.txt", "r", encoding="utf-8-sig") as file:
             word_file_contents = file.read()
 
-            if word[0]["traditional"] not in word_file_contents and (
-                    "simplified" in word[0] and word[0]["simplified"] not in word_file_contents) or \
-                    ("simplified" not in word[0] and word[0]["traditional"] not in word_file_contents):
+            if (not word[0]["simplified"] and word[0]["traditional"] not in word_file_contents) or \
+                (word[0]["traditional"] not in word_file_contents and word[0]["simplified"] not in word_file_contents):
 
                 with open("word_searches.txt", "a+", encoding="utf-8-sig") as word_file:
                     if "simplified" in word[0]:
