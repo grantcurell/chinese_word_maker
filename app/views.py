b3ebac05f3425ce544f31ffe5ae68530c4b9ed91
Grant Curell <grantcurell@gmail.com>
2019-12-24 11:46:46 -0600

    More error handling updates

--- app/views.py
+++ app/views.py
@@ -27,58 +27,61 @@ def _lookup_character():
     get_words_future = executor.submit(get_words, [input_word], app.config["ebook"], select_closest_match=True)
     example_future = executor.submit(get_examples_html, input_word_traditional)
 
-    word, chars = get_words_future.result()
+    if get_words_future.result() is not None:
+        word, chars = get_words_future.result()
 
-    # Make sure that heuristics succeeded and we don't fail to convert for a one to many. This should happen relatively
-    # infrequently so we should still get a net time save.
-    if input_word_traditional != word[0]["traditional"]:
-        logging.warning("We used some heuristics to convert from Simplified to Traditional Chinese. It looks like on "
-                        "further evaluation they failed. This is not fatal and will be automatically fixed, but we must"
-                        " make a new web request which will take a few seconds.")
-        logging.warning("The original search was for " + input_word + " which was converted to " +
-                        input_word_traditional + " but mdbg.net returned " + word[0]["traditional"])
-        example_future = executor.submit(get_examples_html, word[0]["traditional"])
+        # Make sure that heuristics succeeded and we don't fail to convert for a one to many. This should happen relatively
+        # infrequently so we should still get a net time save.
+        if input_word_traditional != word[0]["traditional"]:
+            logging.warning("We used some heuristics to convert from Simplified to Traditional Chinese. It looks like on "
+                            "further evaluation they failed. This is not fatal and will be automatically fixed, but we must"
+                            " make a new web request which will take a few seconds.")
+            logging.warning("The original search was for " + input_word + " which was converted to " +
+                            input_word_traditional + " but mdbg.net returned " + word[0]["traditional"])
+            example_future = executor.submit(get_examples_html, word[0]["traditional"])
 
-    if chars is not None:
+        if chars is not None:
 
-        return get_chars_html(chars, write_character=save_character_checked, server_mode=True)
+            return get_chars_html(chars, write_character=save_character_checked, server_mode=True)
 
-    elif word is not None:
+        elif word is not None:
 
-        logging.info("Performing character lookup for " + word[0]["traditional"])
-        char_future_server = executor.submit(get_chars_html, word[0]["characters"],
-                                             write_character=save_character_checked, server_mode=True)
-        char_future = executor.submit(get_chars_html, word[0]["characters"], image_location=app.config['IMAGE_FOLDER'],
-                                      write_character=save_character_checked, server_mode=False)
+            logging.info("Performing character lookup for " + word[0]["traditional"])
+            char_future_server = executor.submit(get_chars_html, word[0]["characters"],
+                                                 write_character=save_character_checked, server_mode=True)
+            char_future = executor.submit(get_chars_html, word[0]["characters"], image_location=app.config['IMAGE_FOLDER'],
+                                          write_character=save_character_checked, server_mode=False)
 
-        webpage += render_template("word.html", word=word[0]) + "<hr>"
-        webpage += char_future_server.result()
+            webpage += render_template("word.html", word=word[0]) + "<hr>"
+            webpage += char_future_server.result()
 
-        logging.info("Waiting on example to return.")
-        webpage += example_future.result()
+            logging.info("Waiting on example to return.")
+            webpage += example_future.result()
 
-        if app.config["CREATE_COMBINED"]:
-            output_combined_online(word[0], app.config['OUTPUT_FILE'], app.config['DELIMITER'], char_future.result(),
-                                   example_future.result())
+            if app.config["CREATE_COMBINED"]:
+                output_combined_online(word[0], app.config['OUTPUT_FILE'], app.config['DELIMITER'], char_future.result(),
+                                       example_future.result())
+            else:
+                if not path.exists('word_searches.txt'):
+                    with open('word_searches.txt', 'w'):
+                        pass
+
+                with open("word_searches.txt", "r", encoding="utf-8-sig") as file:
+                    word_file_contents = file.read()
+
+                    if (not word[0]["simplified"] and word[0]["traditional"] not in word_file_contents) or \
+                            (word[0]["traditional"] not in word_file_contents and word[0]["simplified"]
+                             not in word_file_contents):
+
+                        with open("word_searches.txt", "a+", encoding="utf-8-sig") as word_file:
+                            word_file.write(word[0]["traditional"] + "\n")
+
+            return webpage
+
         else:
-            if not path.exists('word_searches.txt'):
-                with open('word_searches.txt', 'w'):
-                    pass
-
-            with open("word_searches.txt", "r", encoding="utf-8-sig") as file:
-                word_file_contents = file.read()
-
-                if (not word[0]["simplified"] and word[0]["traditional"] not in word_file_contents) or \
-                        (word[0]["traditional"] not in word_file_contents and word[0]["simplified"]
-                         not in word_file_contents):
-
-                    with open("word_searches.txt", "a+", encoding="utf-8-sig") as word_file:
-                        word_file.write(word[0]["traditional"] + "\n")
-
-        return webpage
-
+            return 'We could not find that character in the book!'
     else:
-        return 'We could not find that character in the book!'
+        return 'Uh oh. The server went and pooped itself. Investigate the logs for more info.'
 
 
 @app.route('/_generate')
