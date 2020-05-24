e2e72cc82cdbf874ef0f6dedf4fa24f64ef93a07
Grant Curell <grantcurell@gmail.com>
2020-05-24 09:51:12 -0500

    Rename

--- chinese_flashcard_maker.py
+++ chinese_flashcard_maker.py
@@ -1,7 +1,7 @@
 __author__ = "Grant Curell"
 __copyright__ = "Do what you want with it"
 __license__ = "GPLv3"
-__version__ = "1.6.1"
+__version__ = "2.0"
 __maintainer__ = "Grant Curell"
 
 from argparse import ArgumentParser
